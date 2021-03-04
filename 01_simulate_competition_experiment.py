63f6c94a1727d4aafb59c8897afc29c69743266f
Steven Shave <stevenshave@gmail.com>
2021-03-04 17:22:51 +0000

    Corrected fraction ligand bound calculation

--- 01_simulate_competition_experiment.py
+++ 01_simulate_competition_experiment.py
@@ -13,11 +13,14 @@ from high_accuracy_binding_equations import *
 # kdpi is the dissociation constant (KD) of the protein-inhibitor interaction
 # We can either expand the dictionary with ** as shown in the example with
 # system1, or we can pass arguments to competition_pl with the following
-# singatur: competition_pl(p, l , i, kdpl, kdpi)
+# singature: competition_pl(p, l , i, kdpl, kdpi)
+
 system1={"p":1, "l":2, "i":10, "kdpl":0.1, "kdpi":10}
 pl_conc=competition_pl(**system1)
-print(f"System 1 - pl_conc = {round(pl_conc,4)}, fraction ligand bound = {round(pl_conc/system1['i'],4)}")
+print(f"pl_conc = {round(pl_conc,4)}, fraction ligand bound = {round(pl_conc/system1['l'],4)}")
 
-# System 2 - line
-print(f"Inline system - pl_conc=", competition_pl(0.2, 0.01, 10, 0.025, 1))
+
+
+
+
 
