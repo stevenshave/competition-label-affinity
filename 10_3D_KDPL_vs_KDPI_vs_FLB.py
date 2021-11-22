8faabbc533cbd70ec1ca90d0a597d3aaf25a75db
Steven Shave <stevenshave@gmail.com>
2021-11-22 10:30:04 +0000

    Tidied axes variables and added best 6.975 pKDPL

--- 10_3D_KDPL_vs_KDPI_vs_FLB.py
+++ 10_3D_KDPL_vs_KDPI_vs_FLB.py
@@ -32,7 +32,7 @@ protein_concs = np.array(calc_amount_p(
     TARGET_FRACTION_L_BOUND, 10e-9, ligand_kds))
 DATAFILENAME="02-data.npz"
 
-y = np.full((ligand_kds.shape[0], inhibitor_kds.shape[0]), np.nan)
+flb = np.full((ligand_kds.shape[0], inhibitor_kds.shape[0]), np.nan)
 if not Path(DATAFILENAME).exists():
     print("Regenerating data file")
     for it_ligand_kds, ligand_kd in enumerate(ligand_kds):
@@ -42,9 +42,9 @@ if not Path(DATAFILENAME).exists():
             lig_conc = 10e-9
             i_conc = 10e-6
             p = protein_concs[it_ligand_kds]
-            y[it_ligand_kds][it_inhibitor_kds] = competition_pl(
+            flb[it_ligand_kds][it_inhibitor_kds] = competition_pl(
                 **{'p': p, 'l': lig_conc, 'i': i_conc, 'kdpl': ligand_kd, 'kdpi': inhibitor_kd})/lig_conc
-    np.savez_compressed(DATAFILENAME, xaxis_beginning=XAXIS_BEGINNING, xaxis_end=XAXIS_END, ligand_kds=ligand_kds,protein_concs=protein_concs,y=y, inhibitor_kds=inhibitor_kds)
+    np.savez_compressed(DATAFILENAME, xaxis_beginning=XAXIS_BEGINNING, xaxis_end=XAXIS_END, ligand_kds=ligand_kds,protein_concs=protein_concs,y=flb, inhibitor_kds=inhibitor_kds)
 
 print("Loading datafile")
 loaded_data=np.load(DATAFILENAME)
@@ -55,14 +55,13 @@ inhibitor_kds=loaded_data['inhibitor_kds']
 protein_concs=loaded_data['protein_concs']
 x_axis=np.linspace(XAXIS_BEGINNING, XAXIS_END, num=ligand_kds.shape[0])
 y_axis=np.linspace(XAXIS_BEGINNING, XAXIS_END, num=inhibitor_kds.shape[0])
-y=loaded_data['y']
-print(y.shape)
+flb=loaded_data['y']
 
 #fig, ax = plt.subplots(2,1, figsize=(7.204724, 5.09424929292), gridspec_kw={'height_ratios':[10,1]})
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(7.204724, 5.09424929292))
 
-X, Z = np.meshgrid(x_axis, y_axis)
-ax.plot_surface(X, Z, y, linewidth=1, cmap=cm.coolwarm,antialiased=False, rstride=10,cstride=10)
+X, Y = np.meshgrid(x_axis, y_axis)
+ax.plot_surface(X, Y, flb, linewidth=1, cmap=cm.coolwarm,antialiased=False, rstride=10,cstride=10, alpha=0.8)
 
 
 
@@ -74,7 +73,10 @@ ax.set_yticklabels(
     ["3 (mM)", "4", "5", r"6 ($\mathrm{\mu}$M)", "7", "8", "9 (nM)", "10", "11", "12 (pM)"])
 ax.set_yticks(range(XAXIS_BEGINNING, XAXIS_END+1))
 
-
+find_index_of_closest_to=lambda val, nparray: np.abs(nparray-val).argmin()
+optimal_y=np.full_like(y_axis, y_axis[find_index_of_closest_to(6.975, y_axis)])
+optimal_flb=flb[find_index_of_closest_to(6.975, x_axis), :]
+ax.scatter(x_axis, optimal_y, optimal_flb, 'k', s=10, alpha=1)
 
 
 ax.set_ylabel(r"Ligand pK$_\mathrm{D}$")
